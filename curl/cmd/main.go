9c1fcc2557a48cac116fd968b1c295e888d8f489
Alex Perez <alejandro.gperez745@gmail.com>
2023-12-30 12:36:03 -0600

    part 1 complete

--- curl/cmd/main.go
+++ curl/cmd/main.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"strings"
 )
-
-func SendReq(url *url.URL) {
-	fmt.Printf("connecting to %s\n", url.Hostname())
-}
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: gocurl <URL>")
+		fmt.Println("Usage: ./bin/gocurl <URL>")
 		return
 	}
 
-	url, err := url.Parse(strings.TrimSpace(os.Args[1]))
+	u := ParseURL(os.Args[1])
+
+	fmt.Fprintf(os.Stdout, "connecting to %s\r\n", u.Hostname())
+	fmt.Fprintf(os.Stdout, "Sending request GET %s HTTP/1.1\r\n", u.Path)
+	fmt.Fprintf(os.Stdout, "Host: %s\r\n", u.Hostname())
+	fmt.Fprintln(os.Stdout, "Accept: */*\r")
+
+}
+
+func ParseURL(rawURL string) *url.URL {
+	u, err := url.Parse(rawURL)
 	if err != nil {
 		panic(err)
 	}
 
-	SendReq(url)
+	if len(u.Port()) == 0 {
+		u.Host = u.Host + ":80"
+	}
+
+	return u
 }
